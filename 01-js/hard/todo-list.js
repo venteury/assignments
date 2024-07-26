3f455ea499e44173c34aec7410243bdc026e5693
Vishal Patel <85049843+venteury@users.noreply.github.com>
2024-07-26 13:29:27 +0530

    todo class

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,30 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todos = [];
+  }
+  add = (todo) => {
+    this.todos.push(todo);
+  };
+  remove = (indexOfTodo) => {
+    if (indexOfTodo < 0 || indexOfTodo >= this.todos.length) return;
+    this.todos.splice(indexOfTodo, 1);
+  };
+  update = (indexOfTodo, updatedTodo) => {
+    if (indexOfTodo < 0 || indexOfTodo >= this.todos.length) return;
+    this.todos[indexOfTodo] = updatedTodo;
+  };
+  getAll = () => {
+    return this.todos;
+  };
+  get = (indexOfTodo) => {
+    if (indexOfTodo < 0 || indexOfTodo >= this.todos.length) return null;
+    return this.todos[indexOfTodo];
+  };
+  clear = () => {
+    this.todos = [];
+  };
 }
 
 module.exports = Todo;
